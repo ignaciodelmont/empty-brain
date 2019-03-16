2fd558ab2c7e2996c3d1be56422c25c0cc44d9db
mati <mati.ratcliffe@gmail.com>
2019-03-16 12:14:09 -0300

    Completed event schema attributes

--- backend/models/event.js
+++ backend/models/event.js
@@ -3,14 +3,44 @@ const Schema = mongoose.Schema;
 
 
 const eventSchema = new Schema({
-    name:{
+    title:{
         type:String,
         required:true
     },
-    lastName:{
+    description:{
+        type:String,
+        required:false
+    },
+    state:{
+        /*
+            Open
+            Running
+            Closed
+        */
         type:String,
         required:true
-    }
+    },
+    type:{
+        /*
+            Evento
+            Suceso
+        */
+        type:String,
+        required:true
+    },
+    startTime:{
+        type:String,
+        required:true
+    },
+    duration:{ // estimated
+        type:String,
+        required:false
+    },
+    location:{
+        type:String,
+        required:true
+    },
+    
 });
 
 let Event = mongoose.model('Event',eventSchema);
