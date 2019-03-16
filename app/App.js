e72fcb3815270911d0f5c775f703d1f26d2dd0f8
mpordomingo <martinpordomingo@hotmail.com>
2019-03-16 00:59:54 -0300

    Add files via upload
    Separe en clases, agregue funcion para distinguir entre ios y android

--- app/App.js
+++ app/App.js
@@ -1,128 +1,184 @@
 import React, { Component } from 'react';
 import { MapView, Location, Permissions } from 'expo';
-import { View, Button, TouchableOpacity, StyleSheet, AppRegistry, Text, Image } from 'react-native';
+import { Platform, SafeAreaView, View, Button, TouchableOpacity, StyleSheet, AppRegistry, Text, Image, Alert } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import { Marker } from 'react-native-maps';
 
-export default class App extends Component {    
-    
-    constructor(props) {
-	super(props);
-	this.onRegionChangeComplete = this.onRegionChangeComplete.bind(this);
-	this._getLocationAsync = this._getLocationAsync.bind(this);
-	this.onChangeText = this.onChangeText.bind(this);
-	this.state = {
-	    region: {latitude: 37.78825,
-		     longitude: -122.4324,
-		     latitudeDelta: 0.0922,
-		     longitudeDelta: 0.0421},
-	    locationResult: null,
-	    location: {coords: { latitude: 37.78825, longitude: -122.4324 }},
-	    userLocation: null,
-	    search:''
-	}; 
-    }
 
-    onChangeText(search) {
-	this.setState({ search });
-//	clearTimeout(this.timeout); // clears old timer
-//	this.timeout = setTimeout(() => this.search(this.search), WAIT_TIME);
+class OSWrapper extends Component {
+  //Permite distinguir entre android y ios para usar el safearea
+  constructor(props){
+    super(props);
+
+  }
+  render(){
+    return  Platform.select({
+      ios: (<SafeAreaView style={this.props.style}>{ this.props.children }</SafeAreaView>),
+      android: (<View style={this.props.style}>{ this.props.children }</View>)
+    });
+  };
+
+}
+
+class EventSearchBox extends Component {
+  constructor(props){
+    super(props);
+    this.onChangeText = this.onChangeText.bind(this)
+    this.state  = { search: '', resultsBoxVisibility: 'none'};
+
+  }
+
+  onChangeText(search) {
+     this.setState({ search });
+     if(search == '')
+       this.setState({ resultsBoxVisibility: 'none'});
+     else
+       this.setState({ resultsBoxVisibility: 'visible'});
+  }
+
+  /*onClear(){
+    this.setState({ search:'' });
+    this.setState({ resultsBoxVisibility: 'none'});
+  }*/
+
+  render(){
+    return (
+      <View style={{
+        alignSelf: 'center',
+        width: '100%',
+        backgroundColor: 'transparent',
+        zIndex:5
+      }}>
+          <SearchBar
+              round
+              style={{backgroundColor: 'red'}}
+              placeholder="Search tags"
+              onChangeText={ this.onChangeText }
+              value = {this.state.search}
+          />
+          <View style={{display: this.state.resultsBoxVisibility, postion:'absolute', backgroundColor:'white', width:'100%', height:'100%'}}>
+            <Text>Hola</Text>
+          </View>
+      </View>
+    )
+  }
+
+
+}
+
+class RightTools extends Component
+{
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+
+    <View style={{
+      position: 'absolute',
+      bottom: '5%',
+      right: '5%' }}>
+        <View >
+
+            <TouchableOpacity onPress={this._getLocationAsync}>
+          <Image style={{height: 60, width: 60}} source={require('./assets/add.png')}/></TouchableOpacity>
+          </View>
+
+
+          <View style={{marginTop:10}}>
+      <TouchableOpacity onPress={this._getLocationAsync}>
+        <Image style={{height: 60, width: 60}} source={require('./assets/location.png')}/></TouchableOpacity>
+      </View>
+    </View>
+    );
     }
+}
+
+export default class App extends Component {
+
+    constructor(props) {
+	     super(props);
+	     this.onRegionChangeComplete = this.onRegionChangeComplete.bind(this);
+	     this._getLocationAsync = this._getLocationAsync.bind(this);
+
+
+	     this.state = {
+	         region: {latitude: 37.788250000001,
+		       longitude: -122.4324,
+		       latitudeDelta: 0.0922,
+		       longitudeDelta: 0.0421},
+	         locationResult: null,
+	         location: {coords: { latitude: 37.78825, longitude: -122.4324 }},
+	         userLocation: null,
+	         search:''
+	        };
+      }
+
     onRegionChangeComplete(region) {
-	this.setState({ region });
-	console.log("Changed region to: ",region); 
+	      this.setState({ region });
+	      console.log("Changed region to: ",region);
     }
 
 
     async _getLocationAsync () {
-//	const { Permissions } = Expo;
-	let { status } = await Permissions.askAsync(Permissions.LOCATION);
-	if (status !== 'granted') {
-	    alert("Enable location!");
-	}
-	let location = await  Location.getCurrentPositionAsync({});
-	console.log(location);
-	let region = {};
-	region.latitude = location.coords.latitude;
-	region.longitude = location.coords.longitude;
-	region.latitudeDelta = this.state.region.latitudeDelta;
-	region.longitudeDelta = this.state.region.longitudeDelta;
-	this.setState({ region });
-	let userLocation = {};
-	userLocation.title = "Location"; 
-	userLocation.description = "userLocation";
-	userLocation.latlng = {latitude:region.latitude , longitude:region.longitude };
-	this.setState({ userLocation });
-    }; 
- 
-    
-    render() {
-	return (	    
-	    <View style = {{ flex: 1 }}>
+      //	const { Permissions } = Expo;
 
+	     let { status } = await Permissions.askAsync(Permissions.LOCATION);
+	     if (status !== 'granted') {
+	        alert("Enable location!");
+	      }
+
+	     let location = await  Location.getCurrentPositionAsync({});
+	     console.log(location);
+
+	      let region = {};
+
+	      region.latitude = location.coords.latitude;
+	      region.longitude = location.coords.longitude;
+	      region.latitudeDelta = this.state.region.latitudeDelta;
+	      region.longitudeDelta = this.state.region.longitudeDelta;
+	      this.setState({ region:region });
+	      let userLocation = {};
+
+	      userLocation.title = "Location";
+	      userLocation.description = "userLocation";
+	      userLocation.latlng = {latitude:region.latitude , longitude:region.longitude };
+        this.setState({ userLocation });
+    };
+
+
+    render() {
+
+	    return (
+        <OSWrapper style={{ flex: 1}}>
+                <EventSearchBox />
+                <View style={{flex:1}}>
                 <MapView
-                    style = {{ flex: 1 }}
-                    provider = { MapView.PROVIDER_GOOGLE }
+                    style = {{ flex: 1, flexDirection:'column' }}
+                    //provider = { MapView.PROVIDER_GOOGLE }
                     region={ this.state.region }
                     onRegionChangeComplete={ this.onRegionChangeComplete }
                     customMapStyle = { generatedMapStyle } >
-                    
+
                     {this.state.userLocation && <MapView.Marker
                         coordinate={this.state.userLocation.latlng}
                         title={this.state.userLocation.title}
                         description={this.state.userLocation.description}
                      />}
-                    
-		</MapView>
-
-
-                <View style={{
-                          position: 'absolute',
-                          top: '3%',
-                          alignSelf: 'center',
-                          width: '95%',
-                          backgroundColor: 'transparent'                          
-                }}>
-                    <SearchBar
-                        round
-                        style={{backgroundColor: 'red'}}
-                        placeholder="Search tags"
-                        onChangeText={ this.onChangeText }
-                        value = {this.state.search}
-                    />
+		            </MapView>
+                <RightTools />
                 </View>
 
+	    </OSWrapper>
 
-                <View style={{
-                          position: 'absolute',
-                          top: '70%',
-                          marginRight: '4%',
-                          alignSelf: 'flex-end',}}>
-                    
-                    <TouchableOpacity onPress={this._getLocationAsync}>
-                        <Image style={{height: 48, width: 50}} source={require('./assets/add.png')}/></TouchableOpacity>
-                </View>
 
-                
-                <View style={{
-                          position: 'absolute',
-                          top: '80%',
-                          marginRight: '4%',
-                          alignSelf: 'flex-end',
-                }}>
-                    <TouchableOpacity onPress={this._getLocationAsync}
-                ><Image style={{height: 50, width: 50}} source={require('./assets/location.png')}/></TouchableOpacity>
-                </View>
-
-	    </View>
-
-              
-	);
-    }
-} 
+	   );
+  }
+}
 
 let region = {latitude: 37.78825,
-	      longitude: -122.4324, 
+	      longitude: -122.4324,
 	      latitudeDelta: 0.0922,
 	      longitudeDelta: 0.0421};
 
