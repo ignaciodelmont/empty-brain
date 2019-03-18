53850cf57cbc93c38f996eb7a5b3741bdb01c5c3
mpordomingo <martinpordomingo@hotmail.com>
2019-03-18 14:02:40 -0300

    Add files via upload

---
+++ app/js/style.js
@@ -0,0 +1,283 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+  result: {
+    marginTop:5,
+    marginLeft:5,
+    marginRight:5,
+    height: 80,
+    flexDirection: 'row',
+    borderBottomWidth:1,
+    borderColor: '#BBB'
+  },
+  resultImage: {
+    height:70,
+    width:70,
+    borderWidth:1,
+    padding:1,
+    borderRadius: 35,
+    borderColor: '#BBB'
+  },
+  close: {
+    marginTop:5,
+    marginRight:18,
+    fontSize:28,
+    color: '#3585FF'
+  },
+  eventName: {
+    width: '50%',
+    fontSize: 25,
+    marginLeft: '3%',
+  },
+  eventState: {
+    marginRight: '3%',
+    marginLeft: '2%',
+    fontSize: 20,
+    color: 'green',
+    width: '20%',
+  },
+  bodyText: {
+    fontSize:18,
+    marginLeft:10,
+    marginRight:5
+  },
+  addForm: {
+    backgroundColor:'white',
+    flex:1,
+    flexDirection:'column'
+  },
+  confirmButton: {
+  },
+  confirmButtonText: {
+    marginTop:40,
+    color:'#2560ff',
+    fontSize:30
+  },
+  red: {
+    color: 'red',
+  },
+  title: {
+    marginLeft:15,
+    fontSize: 45
+  }
+});
+
+const generatedMapStyle = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#242f3e"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#746855"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#242f3e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.business",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#263c3f"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#6b9a76"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#38414e"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#212a37"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9ca5b3"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#746855"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#1f2835"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#f3d19c"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#2f3948"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.station",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#d59563"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#17263c"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#515c6d"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#17263c"
+      }
+    ]
+  }
+];
+
+module.exports = {
+  generatedMapStyle: generatedMapStyle,
+  Styles: styles
+}
